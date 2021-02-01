aaddbe96de834435148fe1e2821dbb1fd5066c14
prabha <Prabhakaran.mails@gmail.com>
2021-01-31 22:40:19 -0500

    First Working code to scrap the data and store it in df

--- web_scrapping/imdb_box_office_weekly.py
+++ web_scrapping/imdb_box_office_weekly.py
@@ -1,16 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-url = 'https://en.wikipedia.org/wiki/2015_in_hip_hop_music'
+url = 'https://en.wikipedia.org/wiki/COVID-19_pandemic_by_country_and_territory'
 
 page = requests.get(url)
 
-#print(page.content)
+
 soup = BeautifulSoup(page.content,'html.parser')
 
-results = soup.find_all('table',class_='wikitable')[1]
+table = soup.find(id='thetable').find('tbody')
 
-for record in soup.find_all('tr'):
-    albumdata=""
-    for data in record.find_all('td'):
-        albumdata = albu,data+","+
+rows = table.find_all('tr')
+
+country = []
+cases=[]
+deaths=[]
+recovered=[]
+
+for row in rows:
+    if len(row)== 12:
+        cells = row.find_all('td')
+        
+        if len(cells)>0:
+            header = row.find_all('th')[1]
+            country.append(header.text.strip())
+            cases.append(cells[0].text.strip().replace(',',''))
+            deaths.append(cells[1].text.strip().replace(',',''))
+            recovered.append(cells[2].text.strip().replace(',',''))
+            
+df = pd.DataFrame({"country":country,"cases":cases,"deaths":deaths,"recovered":recovered})
+print(df)
+    
