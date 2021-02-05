72d545067d18fe5dc46c58d35fe0a941b02b6826
Arivalagan <prabhakaran.mails@gmail.com>
2021-02-04 22:08:55 -0500

    Added updated_ts column

--- data_cleaning/covid.py
+++ data_cleaning/covid.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import feather
 import re
+from datetime import datetime as dt
 
 #reading unclean data
 data = pd.read_feather("../data/covid.ftr")
@@ -11,5 +12,6 @@ data[col] = data[col].apply(lambda x: pd.to_numeric(x,errors='coerce').astype('I
 
 # To remove the subscript text
 data['country'] = data['country'].apply(lambda x:re.sub(r'\[.*\]','',x))
+data['updated_ts']=pd.Series([dt.now()]*len(data))
 
 data.to_csv("test.csv")
